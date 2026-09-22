e746ae265d18dd7b102da98fb3fcefc47f185374
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-09-22 14:06:06 +0545

    working with network interface

--- src/ThridClass/NetInterface.java
+++ src/ThridClass/NetInterface.java
@@ -4,7 +4,6 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Enumeration;
 
